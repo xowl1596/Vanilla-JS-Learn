33339428de4690344d88f3e841a2f0bbdf427a6b
xowl <xowl1596@gmail.com>
2019-03-01 16:29:52 +0900

    add some code

---
+++ Variable/index.js
@@ -0,0 +1,17 @@
+// Create
+let a;
+// Initialize
+a = 221;
+// Use
+console.log(a);
+
+// Create & Initialize
+let b = a - 5;
+// Use
+console.log(b);
+
+// const is can't change value
+const c = 10;
+// It is wrong code
+// c = 13;
+
