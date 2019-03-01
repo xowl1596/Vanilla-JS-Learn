36628ec9a96c44ae29bfc65b11d0cb8a35d8a8b2
xowl <xowl1596@gmail.com>
2019-03-01 16:30:50 +0900

    add some code

---
+++ DataType/index.js
@@ -0,0 +1,10 @@
+// String : text
+const str = "This is string";
+
+// Boolean : true or false
+const boolTrue  = true;
+const boolFalse = false;
+
+// Number : Integer, Float
+const integerNum = 1;
+const floatNum   = 0.5;
