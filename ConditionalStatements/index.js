c4ae16ba6dfdb77467d9a6477297c686cc2e5197
xowl <xowl1596@gmail.com>
2019-03-03 01:11:51 +0900

    add Some code but it's have bug

---
+++ ConditionalStatements/index.js
@@ -0,0 +1,9 @@
+if(10 === 5){
+    console.log("hi");
+}
+else if("10" === "10"){
+    console.log("ho");
+}
+else{
+    console.log("Doh!");
+}
--- DOM/index.js
+++ DOM/index.js
@@ -1,8 +1,29 @@
 //get document object
-const title = document.getElementById("title");
-console.log(title);
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "#34495e";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick(){
+    const currentColor = title.style.color;
+    console.log(currentColor);
+
+    if(currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR
+    }
+    else{
+        title.style.color = BASE_COLOR;
+    }
+}
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+
+init();
 
 //change document object's attribute
-title.innerHTML = "Changed!";
-title.style.color = "red";
-document.title = "chnged title";
+// title.innerHTML = "Changed!";
+// title.style.color = "red";
+// document.title = "chnged title";
