d95b493d3abb27f6cd6c0f81c9b1a56c59fc7a90
xowl <xowl1596@gmail.com>
2019-03-03 21:45:35 +0900

    add code and finish JS tutorial

--- DOM/index.js
+++ DOM/index.js
@@ -1,29 +1,35 @@
 //get document object
 const title = document.querySelector("#title");
+const CLICKED_CLASS = "clicked";
 
-const BASE_COLOR = "#34495e";
-const OTHER_COLOR = "#7f8c8d";
+// const BASE_COLOR = "rgb(52, 73, 94)";
+// const OTHER_COLOR = "#7f8c8d";
 
 function handleClick(){
-    const currentColor = title.style.color;
-    console.log(currentColor);
+    // const currentColor = title.style.color;
+    // console.log(currentColor);
 
-    if(currentColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR
-    }
-    else{
-        title.style.color = BASE_COLOR;
-    }
+    // if(currentColor === BASE_COLOR){
+    //     title.style.color = OTHER_COLOR
+    // }
+    // else{
+    //     title.style.color = BASE_COLOR;
+    // }
+
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if (!hasClass){
+    //     title.classList.add(CLICKED_CLASS);
+    // }
+    // else{
+    //     title.classList.remove(CLICKED_CLASS);
+    // }
+
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init(){
-    title.style.color = BASE_COLOR;
+    // title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 }
 
 init();
-
-//change document object's attribute
-// title.innerHTML = "Changed!";
-// title.style.color = "red";
-// document.title = "chnged title";
